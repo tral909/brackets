0d33224953879a1686afb1914fd06091167f8422
tral909 <indorm1992@mail.ru>
2019-10-02 00:17:02 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+        let stack = [];
+        for (let j = 0; j < str.length; j++) {
+            for (let i = 0; i < bracketsConfig.length; i++) {
+                if (bracketsConfig[i][0] == str[j]) {
+                    stack.push(str[j]);
+                }
+                if (bracketsConfig[i][1] == str[j] && !(bracketsConfig[i][0] == stack.pop())) {
+                    return false;
+                }
+            }
+        }
+        return stack.length == 0;
+    }
